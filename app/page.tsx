8f0deacd13c58dac7bfce9ecd88b405db9582ea7
AxonSkie <axonmatalubos@gmail.com>
2024-07-27 20:21:35 +0800

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,41 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import Link from "next/link";
+
+import myportfolio from "../public/images/myprofile.png";
 
 export default function Home() {
   return (
-    <main className="bg-blue-background min-h-screen">
+    <main className="bg-blue-background min-h-screen ">
       <div>
         <Navbar />
+      </div>
+
+      <div className="flex w-full min-h-[500px] mt-8 flex-col sm:flex-row">
+        <div className="flex flex-1  justify-center">
+          <Image
+            src={myportfolio}
+            className="w-[500px] h-[400px] sm: w-[500px] h-[400px] mr-4"
+          ></Image>
+        </div>
+        <div className="flex flex-1 justify-center items-center  ">
+          <div className="text-white text-[40px] text-center ">
+            <h1>Hello! I'm</h1>
+            <h1 className="text-blue-primary"> Axon Gabriel B. Matalubos</h1>
+            <p className="text-[15px]">
+              Im an aspiring UI/UX designer and specilize in front end
+              development. Im ready to turn your ideas into an existence.
+              Currently a student and a freelancer at the same time
+            </p>
+
+            <Link
+              href="/About"
+              className="mt-5 text-[30px] cursor-pointer text-blue-secondary hover:text-white"
+            >
+              More about me
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import Image from "next/image";
+import logo from "../public/images/logo.png";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="bg-blue-secondary w-full h-20">
-      <ul>
-        <li>
-          <a href="/Contacts">Contacts</a>
-        </li>
-        <li>
-          <a href="/pages">About</a>
-        </li>
-      </ul>
+    <div className="bg-blue-secondary w-full h-20 flex min-h-[100px]">
+      <div className="flex-1 ">
+        <Image src={logo} className=""></Image>
+      </div>
+
+      <div className="flex-1 ">
+        <ul>
+          <li>
+            <Link href="/About">About</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
