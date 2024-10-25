cca88c2829101919417d51f222a3a8c0003d769f
AxonSkie <axonmatalubos@gmail.com>
2024-10-25 15:10:37 +0800

    minor update

--- components/Invert.tsx
+++ components/Invert.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import LaSalle from "../public/gifs/LaSalle.gif";
-import Image from "next/image";
-import Upload from "../public/gifs/Upload.gif";
+// import React from "react";
+// import LaSalle from "../public/gifs/LaSalle.gif";
+// import Image from "next/image";
+// import Upload from "../public/gifs/Upload.gif";
 
-function Invert() {
-  return (
-    <div className=" hidden sm:flex sm:block sm:flex-row sm:flex-1 bg-blue-background ">
-      <div className="flex-1 flex flex-col justify-center hidden sm:flex sm:flex-col  ">
-        <h1 className="text-white text-[50px]">
-          <span className="text-blue-700 text-[50px]">Uploading Photo</span>{" "}
-          System
-        </h1>
-        <p className="mt-6 text-white m-[10px]">
-          The first complete CRUD project I made for my friend's project. The
-          second freelance I took. I used Php, MySQL, HTML and CSS{" "}
-        </p>
-      </div>
-      <div className="flex-1  flex items-center justify-center">
-        {" "}
-        <Image src={Upload} className="w-[500px] sm:w-[800px]" alt="random" />
-      </div>
-    </div>
-  );
-}
+// function Invert() {
+//   return (
+//     <div className=" hidden sm:flex sm:block sm:flex-row sm:flex-1 bg-blue-background ">
+//       <div className="flex-1 flex flex-col justify-center hidden sm:flex sm:flex-col  ">
+//         <h1 className="text-white text-[50px]">
+//           <span className="text-blue-700 text-[50px]">Uploading Photo</span>{" "}
+//           System
+//         </h1>
+//         <p className="mt-6 text-white m-[10px]">
+//           The first complete CRUD project I made for my friend's project. The
+//           second freelance I took. I used Php, MySQL, HTML and CSS{" "}
+//         </p>
+//       </div>
+//       <div className="flex-1  flex items-center justify-center">
+//         {" "}
+//         <Image src={Upload} className="w-[500px] sm:w-[800px]" alt="random" />
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Invert;
+// export default Invert;
--- components/Mainfile.tsx
+++ components/Mainfile.tsx
@@ -8,7 +8,6 @@ import Swipeablee from "@/components/Swipeablee";
 import React from "react";
 import Popup from "@/components/popup";
 import LaSalle from "../public/gifs/LaSalle.gif";
-import Invert from "@/components/Invert";
 import Upload from "../public/gifs/Upload.gif";
 import Mastery from "@/components/Mastery";
 import MasteryContent from "@/components/MasteryContent";
